b7fd2a6f9ba0846fde9966ebe415b2329bdb573f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-01-18 11:19:54 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char_number in range(97, 123):
-    print("{character}".format(character=chr(char_number)), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
