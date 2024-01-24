529e57550ec63479fdfa352e46350a0eb445878d
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-01-24 21:20:45 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,18 +5,17 @@ arg_str_1 = "argument:"
 arg_str_n = "arguments:"
 argc = len(argv) - 1
 
-
 def print_args():
-    for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+  for i in range(1, len(argv)):
+    print("{:d}: {}".format(i, argv[i]))
 
 
 if __name__ == "__main__":
-    if (argc == 0):
-        print("{:d} {}".format(argc, arg_str_0))
-    elif (argc == 1):
-        print("{:d} {}".format(argc, arg_str_1))
-        print_args()
-    else:
-        print("{:d} {}".format(argc, arg_str_n))
-        print_args()
+  if (argc == 0):
+    print("{:d} {}".format(argc, arg_str_0))
+  elif (argc == 1):
+    print("{:d} {}".format(argc, arg_str_1))
+    print_args()
+  else:
+    print("{:d} {}".format(argc, arg_str_n))
+    print_args()
