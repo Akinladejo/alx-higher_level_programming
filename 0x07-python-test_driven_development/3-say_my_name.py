d508699772bb18d8ce7417f4d65adffc3e69160f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-04-05 10:18:54 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+"""
+Module for Name
+"""
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+
+def say_my_name(first_name, last_name=""):
+    """
+    Write a function that prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): string
+        last_name (str:optional): string
+
+    Raises:
+        TypeError: if not string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
