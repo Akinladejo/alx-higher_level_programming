372bf5b2a48a79f0f2a63d6343b9825842aa6425
Akinladejo <Akinladejo.andrew@yahoo.com>
2024-04-05 00:58:05 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A class with and attribute
+"""
+
+class Square:
+    """ a class square with and atr size
+    """
+
+    def __init__(self, size):
+        self.size = size
