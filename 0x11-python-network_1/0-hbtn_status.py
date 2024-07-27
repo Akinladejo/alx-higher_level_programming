f3dfc9f1560903887ae6a38a05b5db353c31b4ee
Akinladejo <akinladejo.andrew@yahoo.com>
2024-07-27 12:03:42 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-from urllib import request, error
-url = "https://alx-intranet.hbtn.io/status"
-try:
-    with request.urlopen(url) as response:
-        res = response.read()
-        print(res)
-except error.HTTPError as err:
-    print("httpError", err)
+"""script to fetch data fro url"""
+
+from urllib.request import Request,urlopen
+from urllib.error import HTTPError, URLError
+if __name__ == "__main__":
+    url = Request("https://alx-intranet.hbtn.io/status")
+    try:
+        with urlopen(url) as response:
+            res = response.read()
+            print("Body response:")
+            print("\t - type: {}",format(type(res)))
+            print("\t - content: {}".format(res))
+            print("\t utf8 content: {}".format(res.decode('utf-8')))
+    except HTTPError as er:
+        print("HTTPError", er)
 
