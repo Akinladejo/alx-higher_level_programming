fb24fdaf03434ccb767ef2b699888e1e951008ac
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-07-29 22:12:58 +0100

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
-from urllib import request, error
+
+"""a Python script that takes in a URL, sends a request to the URL"""
+from urllib import request
 import sys
 
+if __name == "__main__":
+  if len(sys.argv) !=2:
+    print("sys error")
+    sys.exit(1)
+    
+  url = sys.argv[1]
+  
+  try:
+    req = request.Request(url)
+    with req.urlopen(url) as response:
+      res = response.info()
+      x_res = res.get("X-Request-Id")
+      print(x_res)
+  except Exception as e:
+    print("Error", e)
+      
