28ee7dc74e3fd1031230d69da0c99a9e4f964bc1
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-07-29 22:28:03 +0100

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
-
-import urllib.request
 import sys
+from urllib import request
+
+def main():
+    url = sys.argv[1]  # Get the URL from the command-line arguments
+
+    # Send a request to the URL and get the response
+    with request.urlopen(url) as response:
+        # Get the headers from the response
+        headers = response.headers
+
+        # Get the value of the X-Request-Id header
+        x_request_id = headers.get('X-Request-Id')
+
+        # Print the value of the X-Request-Id header
+        print(x_request_id)
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: ./script.py <URL>")
-        sys.exit(1)
+    main()
 
-    url = sys.argv[1]
-
-    try:
-        req = urllib.request.Request(url)
-        with urllib.request.urlopen(req) as response:
-            # Retrieve and print the value of the X-Request-Id header
-            response_headers = response.info()
-            x_request_id = response_headers.get('X-Request-Id')
-            print(x_request_id)
-    except Exception as e:
-        print("Error:", e)
