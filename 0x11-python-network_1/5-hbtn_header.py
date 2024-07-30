82eb0ebf135a357ea6b7c3d482d4d18ae0e616d9
Akinladejo <akinladejo.andrew@yahoo.com>
2024-07-30 07:06:30 -0700

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr//bin/python3
+"""a Python script that takes in a URL, sends a request to the URL"""
+
+import requests
+import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Value Error")
+        sys.exit(1)
+    try:
+        url = sys.argv[1]
+        res = requests.get(url)
+        headers = res.headers
+        print(headers.get('X-Request-Id'))
+    except Exception as e:
+        print("Exception: {}".format(e.code))
+
+if __name__ == "__main__":
+    main()
