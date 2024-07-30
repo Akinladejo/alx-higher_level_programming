22f9ff413d6dc05b05584870c2ba7f60b9e1ec05
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-07-30 15:16:16 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,19 +1,12 @@
-#!/usr//bin/python3
-"""a script that takes in a URL, sends a request to the URL"""
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL,
+and displays the value of the variable X-Request-Id in the response header."""
 
 import requests
 import sys
-def main():
-    if len(sys.argv) != 2:
-        print("Value Error")
-        sys.exit(1)
-    try:
-        url = sys.argv[1]
-        res = requests.get(url)
-        headers = res.headers
-        print(headers.get('X-Request-Id'))
-    except Exception as e:
-        print("Exception: {}".format(e.code))
 
 if __name__ == "__main__":
-    main()
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get("X-Request-Id")
+    print(x_request_id)
