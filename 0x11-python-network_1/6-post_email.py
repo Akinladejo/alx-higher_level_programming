15354b33ff3cff2ef32497b88506e40e7a05d674
Akinladejo <akinladejo.andrew@yahoo.com>
2024-07-31 06:31:32 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address"""
+import sys
+import requests
+
+def main():
+    try:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        req = requests.post(url, data={'email' : email})
+        print(req.text)
+    except Exception as e:
+        print('Error code : {}'.format(e.code))
+
+if __name__ == '__main__':
+    main()
