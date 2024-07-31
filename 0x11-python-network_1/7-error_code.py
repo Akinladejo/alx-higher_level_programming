5854f85b35983e55ce560f2f8104408f4cf6826d
Akinladejo <akinladejo.andrew@yahoo.com>
2024-07-31 06:58:32 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL"""
+"""Python script that takes in a URL, sends a request to the URL, and displays
+the body of the response.
+
+If the HTTP status code is greater than or equal to 400, it prints: Error code:
+followed by the value of the HTTP status code.
+"""
 
 import requests
 import sys
