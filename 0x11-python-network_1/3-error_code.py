84d3630bd4f71edbab6df1b27ea83f388a458275
Akinladejo <akinladejo.andrew@yahoo.com>
2024-07-31 13:25:38 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL """
+
 import sys
 from urllib import parse, request, error
+
 def main():
     url = sys.argv[1]
     try:
         req = request.Request(url)
+
         with request.urlopen(req) as response:
             res = response.read().decode('utf-8')
             print(res)
