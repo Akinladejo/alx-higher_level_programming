0808ecbcaba4457a91d93daee1f7343f0dda2a09
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-08-02 21:26:00 +0100

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""list all previous 10 commits"""
+
+import sys
+import requests
+
+def main():
+  url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+  response = requests.get(url)
+  commit = res.json()
+  try:
+    for i in range(10):
+      print("{}, {}".format(commit[i].get("sha"),
+                            commit[i].get("commit").get("author").get("name")))
+  except IndexError:
+    pass
