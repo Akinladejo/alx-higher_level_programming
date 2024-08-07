c0b27401193f36ddc67c557e170477d13a8a3cd9
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-08-07 19:51:23 +0100

    0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,2 @@
-#!/usr/bin/nodejs
-//JavaScript script that updates the text color of the <header>
-const b = document.querySelector("header");
-b.textcolor = "#FF0000"
+// console.log($('header').hasClass('red'));
+document.querySelector('header').style.color = 'red';
